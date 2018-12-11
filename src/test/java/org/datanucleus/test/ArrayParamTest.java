b778849f9393ebb0a4766a6dc9ffde8c4256c3ea
bkalbfus <bkalbfus@akcourts.us>
2018-12-11 10:52:01 -0900

    Tests pass now.  using .not() instead of .neg() on contains() filter

--- src/test/java/org/datanucleus/test/ArrayParamTest.java
+++ src/test/java/org/datanucleus/test/ArrayParamTest.java
@@ -10,12 +10,15 @@ import java.util.List;
 import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Query;
 import javax.jdo.Transaction;
 import javax.jdo.query.ListExpression;
 
 import org.datanucleus.api.jdo.query.JDOQLTypedQueryImpl;
+import org.datanucleus.query.JDOQLQueryHelper;
 import org.datanucleus.util.NucleusLogger;
 import org.junit.After;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import mydomain.model.OtherPerson;
@@ -24,6 +27,7 @@ import mydomain.model.QPerson;
 
 public class ArrayParamTest
 {
+	
 	
 	@After
 	public void tearDown() {
@@ -38,7 +42,7 @@ public class ArrayParamTest
 	}
 
     @Test
-    public void testArrayParam()
+    public void testArrayParamJDOQLQuery()
     {
         NucleusLogger.GENERAL.info(">> test START");
         PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("MyTest");
@@ -59,20 +63,14 @@ public class ArrayParamTest
 				Person person3 = new Person(3,"Mr Foo3");
 				pm.makePersistent(person3);
             tx.commit();
-            // using primitive int data type causes error
+            // using primitive int data type causes error.  bad omition in Arrays.asList() IMO.  https://stackoverflow.com/a/53730866/1639527
 //            int[] idsList = new int[] {1,3};
             // using wrapped Integer data type works as expected
             Integer[] idsList = new Integer[] {1,3};
             List listIdsList = Arrays.asList(idsList);
             assertTrue("Arrays.asList({an array}) should return a list of the elements in the array!",listIdsList.size() == 2);
-            
-            
-            JDOQLTypedQueryImpl<Person> query = new JDOQLTypedQueryImpl<>(pm, Person.class);
-            QPerson cand = QPerson.candidate();
-            query.filter(
-            		((ListExpression<List<Number>, Number> ) query.listParameter("ids")).contains(cand.id).neg()
-            		);
-            query = (JDOQLTypedQueryImpl<Person>) query.setParameter("ids", Arrays.asList(idsList));
+            Query query = pm.newQuery("SELECT FROM mydomain.model.Person WHERE !:ids.contains(id)");
+            query.setParameters(Arrays.asList(idsList));
             List<Person> result = query.executeList();
             assertNotNull(result);
             assertTrue("result should have the one id not in the list",result.size() == 1);
@@ -97,5 +95,66 @@ public class ArrayParamTest
         pmf.close();
         NucleusLogger.GENERAL.info(">> test END");
     }
+
+	@Test 
+	    public void testArrayParamTypedQuery()
+	    {
+	        NucleusLogger.GENERAL.info(">> test START");
+	        PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("MyTest");
+	
+	        PersistenceManager pm = pmf.getPersistenceManager();
+	        Transaction tx = pm.currentTransaction();
+	        try
+	        {
+	            tx.begin();
+					Person person1 = new Person(1,"Mr Foo");
+					pm.makePersistent(person1);
+	            tx.commit();
+	            tx.begin();
+					Person person2 = new Person(2,"Mr Foo2");
+					pm.makePersistent(person2);
+	            tx.commit();
+	            tx.begin();
+					Person person3 = new Person(3,"Mr Foo3");
+					pm.makePersistent(person3);
+	            tx.commit();
+            // using primitive int data type causes error.  bad omition in Arrays.asList() IMO.  https://stackoverflow.com/a/53730866/1639527
+	//            int[] idsList = new int[] {1,3};
+	            // using wrapped Integer data type works as expected
+	            Integer[] idsList = new Integer[] {1,3};
+	            List listIdsList = Arrays.asList(idsList);
+	            assertTrue("Arrays.asList({an array}) should return a list of the elements in the array!",listIdsList.size() == 2);
+	            
+	            
+	            JDOQLTypedQueryImpl<Person> query = new JDOQLTypedQueryImpl<>(pm, Person.class);
+	            QPerson cand = QPerson.candidate();
+	            query.filter(
+	            		((ListExpression<List<Number>, Number> ) query.listParameter("ids")).contains(cand.id).not()
+	            		);
+	            query = (JDOQLTypedQueryImpl<Person>) query.setParameter("ids", Arrays.asList(idsList));
+	            List<Person> result = query.executeList();
+	            assertNotNull(result);
+	            assertTrue("result should have the one id not in the list",result.size() == 1);
+	            
+	        }
+	        catch (Throwable thr)
+	        {
+	        	System.out.println(thr.getMessage());
+	        	thr.printStackTrace();
+	            NucleusLogger.GENERAL.error(">> Exception in test", thr);
+	            fail("Failed test : " + thr.getMessage());
+	        }
+	        finally 
+	        {
+	            if (tx.isActive())
+	            {
+	                tx.rollback();
+	            }
+	            pm.close();
+	        }
+	
+	        pmf.close();
+	        NucleusLogger.GENERAL.info(">> test END");
+	    }
     
 }
